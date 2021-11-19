709cde936c7ae14767370b6d0eaa9b0299b00086
Valerius Mattfeld <mail@valerius.me>
2021-11-19 13:45:45 +0100

    add database CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IdentityController } from './identity/identity.controller';
 import { IpfsApiService } from './ipfs-api/ipfs-api.service';
+import { DatabasesModule } from './databases/databases.module';
 
 @Module({
-  imports: [],
+  imports: [DatabasesModule],
   controllers: [AppController, IdentityController],
   providers: [AppService, IpfsApiService],
 })
