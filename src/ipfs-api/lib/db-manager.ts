c8af6ec0eb186f65a3dc832e1a5bff57e7696616
Valerius Mattfeld <mail@valerius.me>
2021-11-19 13:46:18 +0100

    update orbitdb-api db-manager

--- src/ipfs-api/lib/db-manager.ts
+++ src/ipfs-api/lib/db-manager.ts
@@ -3,6 +3,10 @@ export class DbManager {
 
   identity = this.orbitdb.identity;
 
+  getOrbitDb() {
+    return this.orbitdb;
+  }
+
   test() {
     return 'Hello from db-manager';
   }
--- src/ipfs-api/lib/orbitdb-api.ts
+++ src/ipfs-api/lib/orbitdb-api.ts
@@ -1,19 +1,43 @@
+import { Logger } from '@nestjs/common';
+import OrbitDB from 'orbit-db';
+import Store from 'orbit-db-store';
 import { DbManager } from './db-manager';
 
 export class OrbitDbApi {
+  private readonly logger = new Logger(OrbitDbApi.name);
+
+  dbs: any = {};
   dbm: any;
+  orbitdb: OrbitDB;
 
   // TODO: #2 implement server options
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   constructor(dbm: DbManager, _serverOptions: any) {
     this.dbm = dbm;
+    this.orbitdb = dbm.getOrbitDb();
   }
 
   identity() {
     return this.dbm.identity;
   }
 
-  test() {
-    return 'Hello from orbitdb-api';
+  findDb(dbName: string): Store | null {
+    // TODO: https://github.com/orbitdb/orbit-db-http-api/blob/develop/src/lib/db-manager.js#L5-L18 Seriously, this is a mess.
+    const result = this.dbs[dbName];
+    return result ? result : null;
+  }
+
+  async get(dbName: string, params?: IOpenOptions) {
+    let db: Store = this.findDb(dbName);
+
+    if (db) return db;
+
+    this.logger.log(`Opening OrbitDB database: ${dbName}`);
+    db = await this.orbitdb.open(dbName, params);
+    await db.load();
+
+    this.dbs[dbName] = db;
+
+    return db;
   }
 }
